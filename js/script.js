6187e09d4a717e4b091c4df3801be0e78d43a9d3
blockitjimjam <blockitjames@terramc.gg>
2025-05-11 23:34:12 +0100

    added right click menu

--- js/script.js
+++ js/script.js
@@ -19,6 +19,44 @@ function showClickMenu(day, time, x, y) {
     const clickMenu = document.getElementById("click-menu");
     clickMenu.style.top = y + 10 + "px";
     clickMenu.style.left = x + 10 + "px";
-    clickMenu.textContent = `${day} at ${time}`
+    clickMenu.innerHTML = `<b>${day} at ${time}</b>`
     clickMenu.style.display = "block"
+    const createEventStartButton = document.createElement("div");
+    createEventStartButton.textContent = "Create Event Starting Here";
+    createEventStartButton.classList.add("click-menu-button");
+    createEventStartButton.onclick = function () {
+        clickMenu.style.display = "none";
+        // createEvent(day, time);
+    };
+    const createEventEndButton = document.createElement("div");
+    createEventEndButton.textContent = "Create Event Ending Here";
+    createEventEndButton.classList.add("click-menu-button");
+    createEventEndButton.onclick = function () {
+        clickMenu.style.display = "none";
+        // createEvent(day, time);
+    };
+    clickMenu.appendChild(createEventStartButton);
+    clickMenu.appendChild(createEventEndButton);
+    // hide the menu if clicked outside
+}
+function createEventPopup(day, time, startOrEnd) {
+    const popup = document.getElementById("popup");
+    popup.style.display = "block";
+    const titleInput = document.getElementById("event-title");
+    const descriptionInput = document.getElementById("event-description");
+    const startTimeInput = document.getElementById("event-start-time");
+    const endTimeInput = document.getElementById("event-end-time");
+
+    titleInput.value = "";
+    descriptionInput.value = "";
+    startTimeInput.value = time;
+    endTimeInput.value = time;
+
+    if (startOrEnd === "start") {
+        endTimeInput.disabled = true;
+        endTimeInput.value = "";
+    } else {
+        startTimeInput.disabled = true;
+        startTimeInput.value = "";
+    }
 }
